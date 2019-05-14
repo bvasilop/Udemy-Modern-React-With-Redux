8b27e778a7c933113bdf8d4388455669bd26959a
bvasilop <bvasilop@gmail.com>
2019-05-14 13:38:06 -0700

    added create-react-practice app

---
+++ practice-app/src/NewComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class NewComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "Bill"
+        }
+
+    }
+    render() {
+        const {name} = this.state;
+        const something = 10;
+        return (
+            <div>New component content {name}
+                <div>New component content {something}</div>
+
+            </div>
+        );
+    }
+}
+export default NewComponent;
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # Udemy Modern React With Redux (Stephen Grider)
+
+If you get stuck at any point in time in this course, you can always compare your code against the completed version of each project.
+
+The completed code for each project can be found here: https://github.com/StephenGrider/redux-code
