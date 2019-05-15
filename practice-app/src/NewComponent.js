460f4b97d4da43e6c74894bc2089f4df09986ec4
bvasilop <bvasilop@gmail.com>
2019-05-15 11:18:26 -0700

    reinstalled yarn packages with eslint configs

--- practice-app/src/NewComponent.js
+++ practice-app/src/NewComponent.js
@@ -4,17 +4,17 @@ class NewComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Bill"
-    }
-
+      name: 'Bill',
+    };
   }
+
   render() {
-    const {name} = this.state;
+    const { name } = this.state;
     const something = 10;
     return (
-      <div>New component content {name}
+      <div>
+        New component content {name}
         <div>New component content {something}</div>
-
       </div>
     );
   }
