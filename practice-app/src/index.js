4763d3e604ebd43e68e5bbc39a1c80e97fa67a77
bvasilop <bvasilop@gmail.com>
2019-05-15 13:19:03 -0700

    added input content and removed create-react app components

--- practice-app/src/index.js
+++ practice-app/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = function() {
+  const buttonText = ' Click Me!';
+  const labelText = 'Enter name: ';
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+        <input id="name" type="text" />
+      </label>
+      <button type="submit" style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'));
