eeb77564aef7e41d92e92146a8ace48a34d8a69e
bvasilop <bvasilop@gmail.com>
2019-05-16 10:51:09 -0700

    added destructuring to props and PropTypes validation

--- components-practice-app/src/CommentDetail.js
+++ components-practice-app/src/CommentDetail.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import faker from 'faker';
+import PropTypes from 'prop-types';
 
-const CommentDetail = () => (
+const CommentDetail = ({ author, timeAgo, post }) => (
   <div className="comment">
     <a href="/" className="avatar">
       <img alt="avatar" src={faker.image.avatar()} />
     </a>
     <div className="content">
       <a href="/" className="author">
-        Sam
+        {author}
       </a>
       <div className="metadata">
-        <span className="date">Today at 6:00PM </span>
+        <span className="date">{timeAgo} </span>
       </div>
-      <div className="text">Nice Blog post!</div>
+      <div className="text">{post}</div>
     </div>
   </div>
 );
+
+CommentDetail.propTypes = {
+  author: PropTypes.string,
+  timeAgo: PropTypes.string,
+  post: PropTypes.string,
+};
 
 export default CommentDetail;
--- components-practice-app/src/index.js
+++ components-practice-app/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import CommentDetail from './CommentDetail.js';
+import CommentDetail from './CommentDetail';
 
 const App = () => (
   <div className="ui container comments">
-    <CommentDetail />
-    <CommentDetail />
-    <CommentDetail />
+    <CommentDetail author="Sam" timeAgo="Today at 4:00PM" post="Hey there" />
+    <CommentDetail
+      author="Alex"
+      timeAgo="Yesterday at 6:00PM"
+      post="What's happening"
+    />
+    <CommentDetail author="Jane" timeAgo="Tomorrow at 2:00PM" post="Hi there" />
   </div>
 );
+
 ReactDOM.render(<App />, document.querySelector('#root'));
