d3a307a4fa98c6b6ea484256901f3ace3e03ae6b
bvasilop <bvasilop@gmail.com>
2019-06-04 20:06:53 -0700

    added event handler for onInputChange

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,15 +1,24 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable jsx-a11y/label-has-for */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 export default class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" placeholder="Search..." />
+            <input
+              type="text"
+              placeholder="Search..."
+              onChange={event => console.log(event.target.value)}
+            />
           </div>
         </form>
       </div>
