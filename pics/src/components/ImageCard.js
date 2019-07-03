dfe704c601e0ccece80b9cc8fc7d454a9ae84d6d
bvasilop <bvasilop@gmail.com>
2019-07-02 19:38:28 -0700

    created imageRed with React.createRef

---
+++ pics/src/components/ImageCard.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+
+  render() {
+    const { description, urls } = this.props.image;
+    return (
+      <div>
+        <img alt={description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+export default ImageCard;
