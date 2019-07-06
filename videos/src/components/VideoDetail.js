df89b6a11b9da557b889321fed1d24e434cd34bd
bvasilop <bvasilop@gmail.com>
2019-07-06 12:50:44 -0700

    added loader div for no video

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -4,7 +4,14 @@ import React from 'react';
 
 const VideoDetail = ({ video }) => {
   if (!video) {
-    return <div>Loading...</div>;
+    // return <div>Loading...</div>;
+    return (
+      <div className="ui segment">
+        <div className="ui active inverted dimmer">
+          <div className="ui text loader">Loading</div>
+        </div>
+      </div>
+    );
   }
 
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
